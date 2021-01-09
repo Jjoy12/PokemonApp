a6ab07335222d3884c09d67833cf3e1bff9a52eb
Jjoy12 <Jerrionjoy@gmail.com>
2021-01-08 22:23:55 -0600

    Utilities folder

---
+++ Utilities/ErrorHandl.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorHandl.swift
+//  PokemonApp
+//
+//  Created by Jerrion Joy on 1/8/21.
+//
+
+import UIKit
+
+enum ErrorHandl:String, Error {
+    case invalidJSON = "Invalid json file"
+}
