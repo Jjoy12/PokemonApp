b3c037467bf506280da11a509b2a012a0b7851ac
Jjoy12 <Jerrionjoy@gmail.com>
2021-01-13 10:48:53 -0600

    API model

---
+++ PokemonModel.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonModel.swift
+//  PokemonApp
+//
+//  Created by Jerrion Joy on 1/12/21.
+//
+
+import UIKit
+
+struct PokData: Codable{
+    var count: Int
+    var next: String
+}
+
